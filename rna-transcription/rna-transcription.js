462255707ad39be2f7f9867b69bc42b0e79f4c42
Mesky <dmeskis@gmail.com>
2019-02-12 13:28:14 -0700

    Rna transcription

--- rna-transcription/rna-transcription.js
+++ rna-transcription/rna-transcription.js
@@ -1,18 +1,13 @@
-const key = {'g': 'c',
-             'c': 'g',
-             't': 'a',
-             'a': 'u'}
-             
-export function toRna(string) {
-  var sequence = string
-  console.log(sequence)
-  if (sequence === "") {
-    return sequence;
-  } else {
-    for (var i = 0; i < sequence.length; i++) {
-      sequence = sequence.replace(sequence[i], key[sequence[i]])
-      debugger;
-    }
-    return sequence
+const key = {'G': 'C',
+             'C': 'G',
+             'T': 'A',
+             'A': 'U'}
+
+export const toRna = (sequence) => {
+  if (sequence === "") { return sequence; }
+  for (var i = 0; i < sequence.length; i++) {
+    sequence = sequence.replace(sequence[i], key[sequence[i]]);
+    // Stuck with a bug where last two characters are not being replaced, help!
   }
+  return sequence;
 }
